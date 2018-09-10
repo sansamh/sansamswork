bcfaf390f68ec9573998460df3d2deb974597181
sansamh <10486712@qq.com>
2018-09-10 17:41:47 +0800

    delete useless

---
+++ my-eureka-client-1/src/main/java/sansam/io/sf/Floor100Eggs2.java
@@ -0,0 +1,10 @@
+package sansam.io.sf;
+
+/**
+ * @version 1.0
+ * @description:
+ * @author: 侯春兵
+ * @Date: 11:04 2018/7/10
+ */
+public class Floor100Eggs2 {
+}
---
+++ my-eureka-client-1/src/main/java/sansam/io/sf/IdWorker.java
@@ -0,0 +1,10 @@
+package sansam.io.sf;
+
+/**
+ * @version 1.0
+ * @description:
+ * @author: 侯春兵
+ * @Date: 11:12 2018/7/24
+ */
+public class IdWorker {
+}
